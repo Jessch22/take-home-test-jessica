5ea980f93b66d2728b6367480672d30ce12d4417
Jessch22 <jessica.535220187@stu.untar.ac.id>
2026-05-10 01:51:36 +0700

    Create dataType.tsx

---
+++ app/users/dataType.tsx
@@ -0,0 +1,43 @@
+export type User = {
+  id:number
+  name:string
+  username:string
+  email:string
+  address: {
+    street:string
+    suite:string
+    city:string
+    zipcode:string
+    geo: {
+      lat:string
+      lng:string
+    }
+  }
+  phone:string
+  website:string
+  company:{
+    name:string
+    catchPhrase:string
+    bs:string
+  }
+}
+
+export type Post = {
+  userId:number
+  id:number
+  title:string
+  body:string
+}
+
+export type Todo = {
+  userId:number
+  id:number
+  title:string
+  completed:boolean
+}
+
+export type EnrichedUser = User & {
+  totalPosts: number
+  completedTodos: number
+  pendingTodos: number
+}
